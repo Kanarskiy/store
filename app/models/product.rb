dc4183d8be4b1b10274a387e0b981af6319af6a4
Alexey Kanarskiy <kanarskiy.oa@gmail.com>
2016-02-12 15:49:08 +0200

    Store and cache of products what was no changed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
     validates :title, presence: true
     validates :description, length: { maximum: 500 }, presence: true
-    validates :price, numericality: {greater_than: 0}, presence: true
+    validates :price, numericality: {greater_than_or_equal_to: 0.01}, presence: true
+
+    def self.latest
+        Product.order(:updated_at).last
+    end
 end
