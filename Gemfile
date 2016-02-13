e119646d32b78bb4fa189b24cde92b790b9af15a
Alexey Kanarskiy <kanarskiy.oa@gmail.com>
2016-02-13 12:40:34 +0200

    start auth

--- Gemfile
+++ Gemfile
@@ -10,14 +10,15 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 
-
 gem 'bootstrap-sass'
 gem 'simple_form'
+
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,24 @@
 class Product < ActiveRecord::Base
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
+
     validates :title, presence: true
-    validates :description, length: { maximum: 500 }, presence: true
+    validates :description, length: {maximum: 500}, presence: true
     validates :price, numericality: {greater_than_or_equal_to: 0.01}, presence: true
+
 
     def self.latest
         Product.order(:updated_at).last
+    end
+
+    private
+    # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+    def ensure_not_referenced_by_any_line_item
+        if line_items.empty?
+            return true
+        else
+            errors.add(:base, 'There are line items in some cart')
+            return false
+        end
     end
 end
